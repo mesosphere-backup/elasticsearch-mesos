9c3b1659ca26315d695059191f49cf8c6d3c5118
Connor Doyle <connor@mesosphere.io>
2014-02-01 15:42:23 -0800

    Removed more cassandra naming artifacts

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -x
 
-# Our elasticsearch-mesos project version follows the Cassandra version number
+# Our elasticsearch-mesos project version follows the Elasticsearch version number
 PROJVERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\[')
-CASSVERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=cassandra.version | grep -v '\[')
+ESVERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=elasticsearch.version | grep -v '\[')
 
-echo Building Cassandra $CASSVERSION for Mesos
+echo Building Elasticsearch $ESVERSION for Mesos
 
 # Create our jar so we can package it up as well. Do this first, so we can fail fast
 mvn clean package
 
 rm -r elasticsearch-mesos-*
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${CASSVERSION}.tar.gz
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${ESVERSION}.tar.gz
 
 tar xzf elasticsearch-*.tar.gz
 rm elasticsearch-*tar.gz
@@ -24,5 +24,3 @@ cp config/* elasticsearch-mesos-${PROJVERSION}/config
 cp target/*.jar elasticsearch-mesos*/lib
 
 tar czf elasticsearch-mesos-${PROJVERSION}.tgz elasticsearch-mesos-${PROJVERSION}
-
-
